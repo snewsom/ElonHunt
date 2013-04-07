f71dec5b9e2f96f1e2bc1a4e2cfb1268ff4b2df9
snewsom <spencer.newsom@gmail.com>
2013-04-07 16:21:51 -0400

    Adding Addplayer and getplayernames stuff

--- src/edu/elon/elonhunt/LocationHideActivity.java
+++ src/edu/elon/elonhunt/LocationHideActivity.java
@@ -1,11 +1,31 @@
 package edu.elon.elonhunt;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.graphics.Color;
+import android.content.Context;
+import android.util.JsonReader;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -29,10 +49,21 @@ public class LocationHideActivity extends Activity {
 		
 		playerText = (EditText) findViewById(R.id.playerText);
 		button = (Button) findViewById(R.id.button);
-		button.setClickable(false);
-		button.setBackgroundColor(Color.GRAY);
-		button.setText("Hide");
+		button.setEnabled(false);
 	}
+	
+	@Override
+	protected void onResume(){
+		super.onResume();
+		locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+	}
+	
+	@Override
+	protected void onPause() {
+		locManager.removeUpdates(locationListener);
+	}
+	
 	
 	
 	private LocationListener locationListener = new LocationListener() {
@@ -67,7 +98,83 @@ public class LocationHideActivity extends Activity {
 	};
 	
 	private void hide() {
-		String name = playerText.toString();
+		//getting player name array to see if the name already exists
+
+			String namesURL = "http://170.224.161.119:9081/elonhunt/getplayers";
+			
+			new GetNames().execute(namesURL);
+			
+		
 	}
+
+	
+	
+	@SuppressLint("NewApi")
+	private class GetNames extends AsyncTask<String,Void,String> {
+
+		@Override
+		protected String doInBackground(String... APIURL) {
+			
+			StringBuilder nameArrayBuilder = new StringBuilder();
+			HttpClient nameClient = new DefaultHttpClient();
+			for(String nameArray : APIURL) {
+				HttpGet nameGet = new HttpGet(nameArray);
+				try {
+					HttpResponse nameResponse = nameClient.execute(nameGet);
+					StatusLine nameStatus = nameResponse.getStatusLine();
+					if(nameStatus.getStatusCode() == 200) { //OK
+						HttpEntity nameEntity = nameResponse.getEntity();
+						InputStream nameContent = nameEntity.getContent();
+						InputStreamReader nameInput = new InputStreamReader(nameContent);
+						BufferedReader nameReader = new BufferedReader(nameInput);
+						
+						JsonReader result = new JsonReader(nameReader);
+						String text = "";
+						
+						//JSON name Array
+						result.beginArray();
+						while(result.hasNext()) {
+							text = result.nextName();
+							if(text.equals("PlayerName")) {
+								nameArrayBuilder.append(result.nextString() + "'");
+							}
+							
+						}
+					}
+				} catch (IOException ex) { }
+					
+				
+			}
+			return nameArrayBuilder.toString();
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+
+			Log.d("tag", result);
+			if(result.contains(playerText.toString())) {
+				playerText.setText("Name already exists");
+			} else {
+				
+				try {
+					String addPlayerMessage = "addplayer?playername=" + playerText.toString() + "&latitude=" + currentLatitude + "&longitude=" + currentLongitude;
+					String encodedString = URLEncoder.encode(addPlayerMessage, "UTF-8");
+					String URL = "http://170.224.161.119:9081/elonhunt/" + encodedString;
+					
+					//dont know if i want to make this another async task or not
+					
+					HttpClient client = new DefaultHttpClient();
+					HttpGet nameGet = new HttpGet(URL);
+					
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			
+		}
+		
+	}
+	
 
 }
